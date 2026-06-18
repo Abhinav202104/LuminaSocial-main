506f81adcfe3c16b1f88a685581264913d1ff973
Abhinav Mishra <152999043+Abhinav202104@users.noreply.github.com>
2026-06-19 00:47:27 +0530

    Update authRoutes.route.js

--- backend/src/routes/authRoutes.route.js
+++ backend/src/routes/authRoutes.route.js
@@ -3,11 +3,8 @@ import { register, login} from "../controllers/authController.controller.js";
 
 const router=express.Router();
 
-
+router.post("/register",register);
 router.post("/login",login);
-router.post('/register', (req, res, next) => {
-    console.log('Register request body:', req.body);
-    next(); // Pass control to the 'register' function below
-}, register);
+
 
 export default router;
